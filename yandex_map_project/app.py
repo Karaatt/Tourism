1f16d92d4a5502a35985394e35e22b33f5d76055
Karaatt <z.don2017@yandex.ru>
2024-06-28 19:40:45 +0300

    Add files via upload

---
+++ yandex_map_project/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+YANDEX_API_KEY = '3de6dadd-fc06-41d7-95b4-cf36885813d9'
+
+coordinates = [
+    "45.044699,41.968149",
+    "45.050061,41.977168",
+    "45.044737,41.961861"
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_route')
+def get_route():
+    url = "https://api.routing.yandex.net/v2/route"
+    params = {
+        "apikey": YANDEX_API_KEY,
+        "waypoints": "|".join(coordinates),
+        "mode": "driving"
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
